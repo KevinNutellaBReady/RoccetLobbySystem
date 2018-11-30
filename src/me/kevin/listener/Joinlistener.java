f64555da5958183a4bd4540504bab0acfa4e0685
KevinNutellaBReady <kevin26032006@gmail.com>
2018-11-30 22:02:33 +0100

    Joinlistener
    Join added

---
+++ src/me/kevin/listener/Joinlistener.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.kevin.listener;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+/**
+ *
+ * @author haral
+ */
+public class Joinlistener implements Listener {
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        e.setJoinMessage(null);
+        Player p = e.getPlayer();
+        
+        
+        
+    }
+    
+}
