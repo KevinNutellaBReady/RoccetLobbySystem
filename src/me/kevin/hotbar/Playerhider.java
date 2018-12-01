72034f5536be7be7dfba3e8ca2dcdb5909c326bb
KevinNutellaBReady <kevin26032006@gmail.com>
2018-12-01 17:46:12 +0100

    Fix

--- src/me/kevin/hotbar/Playerhider.java
+++ src/me/kevin/hotbar/Playerhider.java
@@ -29,8 +29,6 @@ public class Playerhider implements Listener {
 	@EventHandler
 	public void onInteract(PlayerInteractEvent e) {
 		Player p = e.getPlayer();
-		 if(e.getPlayer() == null)return;
-		 
 		 if(e.getAction().equals(Action.RIGHT_CLICK_AIR) || e.getAction().equals(Action.RIGHT_CLICK_BLOCK)){
 	            if(e.getMaterial().equals(Material.BLAZE_ROD)) {
                         if(hidden.contains(p)) {
